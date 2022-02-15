79b432831f21f3b8ab3a7128639a97694dea4af5
Vaishakh G M <mehendalevgm@gmail.com>
2022-02-15 22:07:11 +0530

    Warning functionality

--- vitals/Sensor.java
+++ vitals/Sensor.java
@@ -4,32 +4,38 @@ public class Sensor {
 
     float lowerLimit;
     float upperLimit;
-    float warningPerc;
+    int warningPerc;
+    float WARNING_CONST;
 
     Sensor(float lowerLimit, float upperLimit, int warningPerc){
         this.lowerLimit = lowerLimit;
         this.upperLimit = upperLimit;
         this.warningPerc = warningPerc;
+        this.WARNING_CONST = (this.upperLimit * this.warningPerc) / 100 ;
     }
 
-
-
     protected boolean isWarningApproached(float value){
-        float WARNING_CONST = (this.upperLimit * this.warningPerc) / 100 ;
-        float lowerEnd = this.lowerLimit + WARNING_CONST ;
-        float upperEnd = this.upperLimit - WARNING_CONST ;
 
+        return islowerWarningReached(value) || isUpperWarningReached(value);
+
+    }
+
+    protected boolean islowerWarningReached(float value){
+        float lowerEnd = this.lowerLimit + this.WARNING_CONST ;
         if( value <= lowerEnd ){
             PrintEngine.printWarningCode(1, this.getClass().getName());
             return true;
         }
+        return false;
+    }
+
+    protected boolean isUpperWarningReached(float value){
+        float upperEnd = this.upperLimit - this.WARNING_CONST ;
         if( value >= upperEnd){
             PrintEngine.printWarningCode(2, this.getClass().getName());
             return true;
         }
-
         return false;
-
     }
 
     protected boolean isInRange(float value){
